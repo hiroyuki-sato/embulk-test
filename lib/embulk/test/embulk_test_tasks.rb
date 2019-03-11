7a36fc43bc2ce350c4431bd400caf156c6b4e3a5
Hiroyuki Sato <hiroysato@gmail.com>
2019-03-11 22:16:55 +0900

    Download embulk.jar into vendor/embulk/embulk.jar

--- lib/embulk/test/embulk_test_tasks.rb
+++ lib/embulk/test/embulk_test_tasks.rb
@@ -1,15 +1,19 @@
 namespace :embulk do
   namespace :test do
-    desc "test"
+    desc "Download embulk.jar file"
     task :download_embulk do
       require 'open-uri'
+      require 'fileutils'
       url = "https://dl.embulk.org/embulk-latest.jar"
       
+      print "Downloading #{url} ..."
+      FileUtils.mkdir_p "vendor/embulk"
       open(url) do |file|
-        open("embulk.jar", "wb") do |out|
+        open("vendor/embulk/embulk.jar", "wb") do |out|
           out.write(file.read)
         end
       end
+      puts "Done"
 
     end
   end
