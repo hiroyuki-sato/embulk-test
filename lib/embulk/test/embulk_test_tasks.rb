0c73b08552c99b5fa8797172a9c854071e3df58b
Hiroyuki Sato <hiroysato@gmail.com>
2019-03-12 10:16:20 +0900

    Add specific version download

--- lib/embulk/test/embulk_test_tasks.rb
+++ lib/embulk/test/embulk_test_tasks.rb
@@ -1,10 +1,18 @@
 namespace :embulk do
   namespace :test do
     desc "Download embulk.jar file"
-    task :download_jar do
+    task :download_jar, [:version] do |task,args|
+      LATEST_URL = "https://dl.embulk.org/embulk-latest.jar"
       require 'open-uri'
       require 'fileutils'
-      url = "https://dl.embulk.org/embulk-latest.jar"
+      version = args[:version]
+      if version.nil?
+        url = LATEST_URL
+      elsif version =~ /\A\d+\.\d+\.\d+\z/
+        url = "https://github.com/embulk/embulk/releases/download/v#{version}/embulk-#{version}.jar"
+      else
+        raise ArgumentError,"Invalid version format"
+      end
       
       print "Downloading #{url} ..."
       FileUtils.mkdir_p "vendor/embulk"
